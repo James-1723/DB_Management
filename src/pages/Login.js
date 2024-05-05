f5f68a17cdcb9970b0972e86370742f44ef2c475
wanrou1109 <111306038@g.nccu.edu.tw>
2024-05-05 11:22:40 +0800

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
     return ( 
         
         <div className="login-container">
-            <form className='login-form' onSubmit="handleLogin">
+            <form className='login-form'>
                 <h2>Login</h2>
                 <div className='input-container'>
                     <p>Your Email</p>
