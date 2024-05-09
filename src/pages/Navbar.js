b55fb2d5579c868f05b7ecdd465f1826fbc571af
wanrou1109 <111306038@g.nccu.edu.tw>
2024-05-09 15:54:27 +0800

    navbar.js加圖

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useUser } from '../context/UserContext.js'
 import '../style/Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
 
@@ -19,9 +21,15 @@ const Navbar = () => {
                 <input type="text" placeholder="輸入文字..."/>
                 <button>搜尋</button>
             </form>
-            <div className="links">
-                <Link to='/'>篩選</Link>
-                <Link to='/create'>建立</Link>
+            <div className='links'>
+                <div className='filter-link'>
+                    <Link to='/'>
+                        <FontAwesomeIcon icon={faFilter}/> 篩選
+                    </Link>
+                </div>
+                <div className='create-link'>
+                    <Link to='/create'>建立</Link>
+                </div>
                 {
                     user
                     ? <Link to='/user'>Personal</Link>
