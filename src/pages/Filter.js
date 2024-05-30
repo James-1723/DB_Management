9f93f43ab787fca0f9619c54a809a00c174a4217
wanrou1109 <111306038@g.nccu.edu.tw>
2024-05-30 14:19:59 +0800

    filter modifying

--- src/pages/Filter.js
+++ src/pages/Filter.js
@@ -1,11 +1,50 @@
-
 import '../style/Filter.css'
 
 const Filter = () => {
 
     return (
-        <div className='fiiter-container'>
-            
+        <div className='filter-container'>
+            <h2>Filter</h2>
+            <div className='filter-input'>
+                <p>樣式</p>  
+                <Select
+                    value={selectedTags}
+                    onChange={handleChange}
+                    onInputChange={handleInputChange}
+                    options={options}
+                    isClearable
+                    isSearchable
+                    isMulti
+                    placeholder="Enter or select tags"
+                />
+            </div>
+            <div className='filter-input'>
+                <p>鍋具</p>
+                <Select
+                    value={selectedTags}
+                    onChange={handleChange}
+                    onInputChange={handleInputChange}
+                    options={options}
+                    isClearable
+                    isSearchable
+                    isMulti
+                    placeholder="Enter or select tags"
+                />
+            </div>
+            <div className='filter-input'>
+                <p>食材</p>
+                <Select
+                    value={selectedTags}
+                    onChange={handleChange}
+                    onInputChange={handleInputChange}
+                    options={options}
+                    isClearable
+                    isSearchable
+                    isMulti
+                    placeholder="Enter or select tags"
+                />
+            </div>
+            <button className="filter-button" onClick={filterPost}>篩選</button>
         </div>
     );
 }
