fe832856054cb236de6fde50347136023252a914
James-1723 <69380745+James-1723@users.noreply.github.com>
2024-05-30 11:11:11 +0800

    Finished Post api
    1. Create file:
        - PostHandler
    2. Modified file:
        - schema.sql: add 'title'

--- .history/database/schema_20240530105340.sql
+++ .history/database/schema_20240530105340.sql
@@ -7,6 +7,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `user_id_UNIQUE` (`user_id`)
 );
 
+CREATE TABLE IF NOT EXISTS  `post` (
+  `post_id` int NOT NULL,
+  `tag_name` varchar(255) NOT NULL,
+  `tag_type` enum('ingredient','cooker','type') NOT NULL,
+  PRIMARY KEY (`post_id`,`tag_name`),
+  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`)
+);
+
 CREATE TABLE IF NOT EXISTS `report` (
   `user_id` int NOT NULL,
   `post_id` int NOT NULL,
