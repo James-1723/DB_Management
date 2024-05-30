d0fcdb463bf6d14ea83adb5802c08ad4a2777206
eason9338 <9338akira@gmail.com>
2024-05-30 13:55:10 +0800

    修正schema.sql
    修正資料庫語法錯誤

--- .history/database/test_post_20240516160523.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `test_post` (
-  `post_ID` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) NOT NULL,
-  `content` varchar(90) NOT NULL,
-  `value`  int NOT NULL,
-  `label` varchar(10) NOT NULL
-  PRIMARY KEY (`post_ID`)
-);
