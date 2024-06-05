6b6852d7a8377290733780c8b0eef96c9e939b3b
eason9338 <9338akira@gmail.com>
2024-06-05 16:22:32 +0800

    search by tag

--- database/schema.sql
+++ database/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `image` (
 CREATE TABLE IF NOT EXISTS  `post` (
   `title` varchar(225) NOT NULL,
   `post_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
+  `user_id` int NOT NULL ,
   `content` varchar(200) DEFAULT NULL,
   `share_tag` int DEFAULT NULL,
   `like_tag` int DEFAULT NULL,
--- express/api/postHandler.js
+++ express/api/postHandler.js
@@ -57,19 +57,17 @@ router.get('/posts', (req, res) => {
     });
 });
 
+
 router.post('/post', async (req, res) => {
     const { title, content, selectedTags, selectImage, user_id } = req.body;
 
     try {
-        // Insert image and get imgId
         const imgResults = await db.promise().query('INSERT INTO image (image) VALUES (?)', [selectImage]);
         const imgId = imgResults[0].insertId;
 
-        // Insert post
         const postResults = await db.promise().query('INSERT INTO post (title, content, user_id, image_id) VALUES (?, ?, ?, ?)', [title, content, user_id, imgId]);
         const postId = postResults[0].insertId;
 
-        // Insert tags
         await Promise.all(selectedTags.map(tag => {
             return db.promise().query('INSERT INTO post_tag (post_id, tag_id) VALUES (?, ?)', [postId, tag.value]);
         }));
@@ -78,6 +76,59 @@ router.post('/post', async (req, res) => {
     } catch (err) {
         console.error('Error during post creation: ', err);
         res.status(500).json({ success: false, message: 'Error during post creation' });
+    }
+});
+
+router.post('/searchByTags', async (req, res) => {
+    const { tags } = req.body; 
+
+    if (!tags || tags.length === 0) {
+        res.status(400).json({ success: false, message: 'No tags provided' });
+        return;
+    }
+
+    try {
+
+        const tagQuery = `
+        SELECT 
+            post.post_id, 
+            post.title AS post_title, 
+            post.content AS post_content, 
+            user.user_name,
+            (
+                SELECT GROUP_CONCAT(t.tag_name SEPARATOR ', ')
+                FROM post_tag pt
+                JOIN tag t ON pt.tag_id = t.tag_id
+                WHERE pt.post_id = post.post_id
+            ) AS tags
+        FROM post
+        JOIN user ON post.user_id = user.user_id
+        JOIN post_tag pt ON pt.post_id = post.post_id
+        JOIN tag t ON pt.tag_id = t.tag_id
+        WHERE t.tag_name IN (?)
+        GROUP BY post.post_id
+    
+        `;
+
+        const [results, fields] = await db.promise().query(tagQuery, [tags.join(',')]);
+
+        if (results.length > 0) {
+            console.log(results.length);
+            console.log('results: ', results);
+            const posts = results.map(post => ({
+                post_id: post.post_id,
+                post_title: post.post_title,
+                post_content: post.post_content,
+                user_name: post.user_name
+            }));
+            res.status(200).json({ success: true, posts });
+        } else {
+            res.status(404).json({ success: false, message: 'No posts found with the provided tags', posts: []});
+        }
+    } catch (err) {
+        console.error('Error fetching posts by tags: ', err);
+        res.status(500).send('Server error');
+    
     }
 });
 
